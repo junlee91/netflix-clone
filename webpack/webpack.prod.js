b15464ef90d7122e866d1acb2723a15c03bf4394
junlee91 <jlee757@uic.edu>
2018-06-24 18:41:16 -0500

    webpack production config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,2 +1,40 @@
 const path = require("path");
 const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const PATHS = {
+  build: path.join(__dirname, "../dist")
+};
+
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                minimize: true
+              }
+            },
+            {
+              loader: "postcss-loader"
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(PATHS.build, {
+      root: process.cwd()
+    }),
+    new ExtractTextPlugin("styles.css")
+  ]
+};
