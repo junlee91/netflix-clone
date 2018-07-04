2eeccbbe37d10df7263742a3e9b2e80afb3d83f4
junlee91 <jlee757@uic.edu>
2018-07-03 23:39:39 -0500

    Box using grid and previous&next sibling

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ const header = document.querySelector(".js-header"),
   video = document.querySelector(".js-video"),
   muteBtn = document.querySelector(".js-muteBtn"),
   playBtn = document.querySelector(".js-playBtn"),
-  range = document.querySelector(".js-range");
-  
-video.autoplay = true;
-video.loop = true;
+  range = document.querySelector(".js-range"),
+  boxes = document.querySelectorAll(".box");
 
+// Default volume setting
 const loadMutedPreference = () => {
   const mutedPref = localStorage.getItem("muted");
   if (mutedPref !== null) {
@@ -25,9 +24,9 @@ const loadMutedPreference = () => {
   }
 };
 
+// ------------------------ Header Settings ------------------------
 const handleScroll = event => {
   const scrollHeight = window.scrollY;
-
   // Top Header
   if (scrollHeight > 50) {
     header.classList.add("black");
@@ -43,8 +42,9 @@ const handleScroll = event => {
   }
 };
 
+// ------------------------ Video Settings ------------------------
 const handleMuteBtnClick = () => {
-  if(video.muted) {
+  if (video.muted) {
     video.muted = false;
     muteBtn.innerHTML = "Mute";
     localStorage.setItem("muted", false);
@@ -53,8 +53,7 @@ const handleMuteBtnClick = () => {
     muteBtn.innerHTML = "Unmute";
     localStorage.setItem("muted", true);
   }
-}
-
+};
 const handlePlayBtnClick = () => {
   if (video.paused) {
     playBtn.innerHTML = "⏸";
@@ -64,18 +63,57 @@ const handlePlayBtnClick = () => {
     video.pause();
   }
 };
-
 const handleRageChange = event => {
   const currentVolume = event.target.value;
   video.volume = currentVolume;
 };
 
+// ------------------------ Box Settings ------------------------
+const boxArray = Array.from(boxes);
+
+const findPreviousBoxes = element => {
+  const foundPreviousBoxes = [];
+  const findPrevious = element => {
+    if (element !== null) {
+      foundPreviousBoxes.push(element);
+    }
+  };
+  findPrevious(element.previousSibling);
+};
+
+const handleBoxMouseOver = event => {
+  const box = event.target;
+  const previousOne = box.previousElementSibling;
+  const nextOne = box.nextElementSibling;
+
+  previousOne.classList.add("previous");
+  nextOne.classList.add("next");
+};
+
+const handleBoxMouseLeave = event => {
+  const box = event.target;
+  const previousOne = box.previousElementSibling;
+  const nextOne = box.nextElementSibling;
+
+  previousOne.classList.remove("previous");
+  nextOne.classList.remove("next");
+};
+
+// ------------------------ Listener Settings ------------------------
 const handleContentLoaded = () => {
   window.addEventListener("scroll", handleScroll);
   muteBtn.addEventListener("click", handleMuteBtnClick);
   playBtn.addEventListener("click", handlePlayBtnClick);
   range.addEventListener("change", handleRageChange);
-}
+
+  boxArray.forEach(box => {
+    box.addEventListener("mouseover", handleBoxMouseOver);
+    box.addEventListener("mouseleave", handleBoxMouseLeave);
+  });
+
+  video.autoplay = true;
+  video.loop = true;
+};
 loadMutedPreference();
 
 document.addEventListener("DOMContentLoaded", handleContentLoaded);
